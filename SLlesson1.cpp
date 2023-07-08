383177d76e333fd7a6a12e82068d2476c7521556
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-07-08 00:06:09 -0400

    Add files via upload

---
+++ SLlesson1.cpp
@@ -0,0 +1,29 @@
+// name : variables.cpp
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x; // int is short for integer, our variable. any whole number, no decimals.
+
+	int camelCaseVariable; // all statements end with a ';' 
+	
+	x = 10;
+
+	char c = 'A'; // character valuse with single quotes please!
+
+	float pi = 3.14; // real values with decimals 
+
+	double doublePI = 3.14159; 
+
+	cout << "the value of x is: " << x << endl; 
+
+	cout << "the value of c is: " << c << endl; 
+
+	cout << "the value of pie is: " << pi << endl; 
+
+	cout << "the value of double pie is: " << doublePI << endl; 
+
+	return 0; 
+}
