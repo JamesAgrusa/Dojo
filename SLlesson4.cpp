63baacc3be57e2290c5a470a617ca81cc05a30bd
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-07-08 23:08:49 -0400

    Add files via upload

---
+++ SLlesson4.cpp
@@ -0,0 +1,17 @@
+// basic inputs
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char charInput;
+
+	cout << "Please enter a character: ";
+	cin >> charInput;
+
+	cout << "The character you inputted is: " << charInput << endl;
+
+	
+	return 0;
+}
