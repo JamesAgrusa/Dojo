087bdf64efafe33fe251909678ea5647c9517d2f
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-07-08 23:22:28 -0400

    Add files via upload

---
+++ SLlesson4b.cpp
@@ -0,0 +1,21 @@
+// basic inputs cont. 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	float fInput;
+	float fInput2;
+
+	cout << "Please enter 2 numbers with a space between:  ";
+
+	cin >> fInput >> fInput2;
+
+	cout << "The first number you entered was: " << fInput << endl;
+	cout << "The seccond number you entered was: " << fInput2 << endl;
+	
+
+	return 0;
+}
