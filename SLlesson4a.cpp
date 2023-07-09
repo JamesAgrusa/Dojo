54747fc037c7251b78e346ffeadd54ed12d0d2ca
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-07-08 23:14:43 -0400

    Add files via upload

---
+++ SLlesson4a.cpp
@@ -0,0 +1,22 @@
+// basic inputs cont. 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int intInput;
+	// float fInput
+
+	cout << "Please enter a number: ";
+    
+	cin >> intInput;
+	// cin >> fInput
+
+	cout << "You entered: " << intInput << endl;
+	// cout << "You entered: " << fInput << endl;
+	// this code will allow for a decimal input
+
+
+	return 0;
+}
