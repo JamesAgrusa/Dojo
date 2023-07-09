2b22d6a1132b16f86933ea82b55daf6e297a504d
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-07-08 22:34:35 -0400

    Add files via upload

---
+++ SLlesson3.cpp
@@ -0,0 +1,28 @@
+// arithmetic operations 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x = 1;
+	int y = 2;
+
+	// x = x + 1;
+
+	// x++;  same result as above also works as "x--" to reduce by 1. 
+
+	// x += 1; also same. 
+
+	// int z = x - y, int z = x * y, int z = x / y
+
+	int z = x + y;
+
+	cout << "the value z is: " << z << endl;
+
+
+
+
+	return 0;
+}
+
