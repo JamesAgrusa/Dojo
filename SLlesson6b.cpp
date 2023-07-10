7d4dbe83bdfde36b3fdffbfdf101c6a1a5a43d91
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-07-09 21:40:19 -0400

    Add files via upload

---
+++ SLlesson6b.cpp
@@ -0,0 +1,21 @@
+// conditions
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	bool A = true;
+	bool B = false;
+
+	int x = 10;
+	int y = 0;
+
+	// bool result = (A && B) || (x > y); (true)
+	// bool result = (!A || B) && (x <= y); (false)
+	// bool result = !((A == B) && (x == y)); (true)
+
+
+	cout << "The value of the result is: " << result << endl;
+
+	return 0;
+}
