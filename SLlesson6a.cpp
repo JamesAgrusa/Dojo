4e640279074aaa1140eb391815a01e1e3617ef7d
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-07-09 21:34:49 -0400

    Add files via upload

---
+++ SLlesson6a.cpp
@@ -0,0 +1,14 @@
+// conditions
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	bool myBoolean = false; 
+
+	bool result = true || false ; // && - and || - or
+
+	cout << "The value of the result is: " << result << endl;
+
+	return 0;
+}
