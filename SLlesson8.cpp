64f13163f230cf0e2c729d115795370a5fb7db7a
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-07-09 23:59:12 -0400

    Add files via upload

---
+++ SLlesson8.cpp
@@ -0,0 +1,29 @@
+// switch statements prior to any input
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x;
+	char c;
+	bool b;
+
+
+	switch (expression)
+	{
+	case constant-expression:
+	//statments go here!
+	break; // optional
+
+	case constant-expression:
+	//statement
+	break;
+	default:
+		// catch all
+		// default statement 
+	}
+
+
+
+	return 0;
+}
