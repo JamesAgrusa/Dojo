ba79e471407fe32ec9d9b822df38b6921f238172
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-07-10 22:39:55 -0400

    Add files via upload

---
+++ SLlesson10.cpp
@@ -0,0 +1,21 @@
+// while loops
+
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+
+	int i = 1;
+	const int MAX = 10; 
+
+	while ( i <= MAX)
+	{
+		cout << "The current iteration is: " << i << endl;
+
+		i++;
+	}
+
+
+	return 0;
+}
