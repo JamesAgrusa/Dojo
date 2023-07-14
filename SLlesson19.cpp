30b8951ce89a9b74808e6002a3b6cb85a6344bff
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-07-13 22:22:28 -0400

    Add files via upload

---
+++ SLlesson19.cpp
@@ -0,0 +1,20 @@
+// functions that return values
+
+#include <iostream>
+using namespace std;
+
+int Square(int x);
+
+int main()
+{
+	
+	
+	cout << "The square of 5 is: " << Square(5) << endl;
+
+	return 0;
+}
+
+int Square(int x);
+{
+	return x * x;
+}
