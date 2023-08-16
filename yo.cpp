4037e751f4e5b0d9e10b7184b4c44ad03a330656
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-08-15 21:24:25 -0400

    Add files via upload

---
+++ yo.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+
+/*
+this code will show basic things
+such as using # incldue
+
+or printing things to the screen
+*/
+
+int main()
+{
+    // this will print my name to the screen
+    printf("Hello, my name is James.\n ");
+    // this will pring the following:
+    printf("Programming is really cool.\n");
+    //you get the idea
+    printf("I am very excited about this new journey\n");
+}
+
+/*
+to run a code in visual code vs visual stuido hit:
+...
+then terminal
+run build task 
+then in there type ""
+.\"your file name."
+*/
