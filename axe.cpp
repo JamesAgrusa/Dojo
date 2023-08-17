682d2f49c80c2cb633d46203aebfa3a516586c2e
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-08-16 22:37:59 -0400

    Add files via upload

---
+++ axe.cpp
@@ -0,0 +1,15 @@
+#include "raylib.h"
+int main()
+{
+    float root_beer = 1.99;
+    
+    double cheese_burger{5.99};
+    
+    bool shouldHaveLunch{};
+    
+
+    int width;
+    width = 350;
+    InitWindow(width, 200, "James' Window");
+
+}
