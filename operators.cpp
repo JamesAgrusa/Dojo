c0b93521625a0ff3a6004d1c9b1146983c367b21
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-08-16 22:55:54 -0400

    Add files via upload

---
+++ operators.cpp
@@ -0,0 +1,17 @@
+#include "raylib.h"
+int main()
+{
+    // false
+    bool equal{ 4 == 9 };
+    // true
+    bool notEqual{ 4 != 9 };
+    // true
+    bool less{ 4 < 9 };
+    // false
+    bool greater{ 4 > 9 };
+    // true
+    bool lessEqual{ 4 <= 9 };
+    // false
+    bool greaterEqual{ 4 >= 9 };
+
+}
