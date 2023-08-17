8353848eb0d2f72ef48c112cd5180b473d127409
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-08-16 23:06:57 -0400

    Add files via upload

---
+++ loop.cpp
@@ -0,0 +1,14 @@
+// loops repeat code until conditions are met
+// loop condition is a bool expression and will look for a truth value, until false
+#include "raylib.h"
+int main()
+{
+    int width;
+    width = 350;
+    InitWindow(width, 200, "James' Window");
+
+    while (true)
+    {
+
+    }
+}
