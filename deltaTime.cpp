5c73208fc1f2ce3769044a0bb5972c896c04bcb1
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-08-22 21:19:10 -0400

    Add files via upload

---
+++ deltaTime.cpp
@@ -0,0 +1,67 @@
+#include "raylib.h"
+
+int main()
+{
+    //window dimensions
+    const int windowWidth{512};
+    const int windowHeight{380};
+    // initialize the window
+    InitWindow(windowWidth, windowHeight, "Dapper Dasher!");
+
+    // acceleration due to gravity (pixels/s)/s
+    const int gravity{1'000};
+
+    Texture2D scarfy = LoadTexture("textures/scarfy.png");
+    Rectangle scarfyRec;
+    scarfyRec.width = scarfy.width/6;
+    scarfyRec.height = scarfy.height;
+    scarfyRec.x = 0;
+    scarfyRec.y = 0;
+    Vector2 scarfyPos;
+
+    
+    scarfyPos.x = windowWidth/2 - scarfyRec.width/2;
+    scarfyPos.y = windowHeight - scarfyRec.height;
+
+    // is the rectanlge in the air?
+    bool isInAir{};
+    // jump velocity (pixels / second)
+    const int jumpVel{-600};
+    int velocity{0};
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+//delta time(time since last frame)
+const float dT{GetFrameTime()};
+
+        // start drawing
+        BeginDrawing();
+        ClearBackground(WHITE);
+        // perform ground check 
+        if (scarfyPos.y >= windowHeight - scarfyRec.height)
+        {
+            // rectangle is on the ground
+            velocity = 0;
+            isInAir = false;
+        }
+        else
+        {
+            // rectangle is in the air
+            velocity += gravity * dT;
+            isInAir = true;
+        }
+        // jump check
+        if (IsKeyPressed(KEY_SPACE) && !isInAir)
+        {
+            velocity += jumpVel;
+        }
+        // update the position
+        scarfyPos.y += velocity * dT;
+
+        DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
+        // stop drawing
+        EndDrawing();
+    }
+    UnloadTexture(scarfy);
+    CloseWindow();
+}
