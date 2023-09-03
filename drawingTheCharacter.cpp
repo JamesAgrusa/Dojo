4148599ab7756639548cb1868b2f3413ce60f0e5
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-03 11:34:57 -0400

    Add files via upload

---
+++ drawingTheCharacter.cpp
@@ -0,0 +1,49 @@
+#include "raylib.h"
+#include "raymath.h"
+
+int main()
+{
+    const int windowWidth{384};
+    const int windowHeight{384};
+    InitWindow(windowWidth, windowHeight, "James' Top Down");
+
+    Texture2D map = LoadTexture("nature_tileset/WorldMap.png");
+    Vector2 mapPos{0.0, 0.0};
+    float speed{4.0};
+
+    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
+    Vector2 knightPos{
+        (float)windowWidth/2.0f - 4.0f * (0.5f * (float)knight.width/6.0f), // c style cast. turning width 'int' into a 'float'
+        (float)windowHeight/2.0f - 4.0f *  (0.5f * (float)knight.height)
+    };
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        
+        Vector2 direction{};
+        if (IsKeyDown(KEY_A)) direction.x -= 1.0;
+        if (IsKeyDown(KEY_D)) direction.x += 1.0;
+        if (IsKeyDown(KEY_W)) direction.y -= 1.0;
+        if (IsKeyDown(KEY_S)) direction.y += 1.0;
+        if (Vector2Length(direction) != 0.0)
+        {
+            // set mapPos = mapPos - direction
+            
+            mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+        }
+        
+        // draw the map
+        DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
+        // draw the character
+        Rectangle source{0.f, 0.f, (float)knight.width/6.f, (float)knight.height};
+        Rectangle dest{knightPos.x, knightPos.y, 4.0f * (float)knight.width/6.0f, 4.0f * (float)knight.height};
+        DrawTexturePro(knight, source, dest, Vector2{}, 0.f, WHITE);
+
+        EndDrawing();
+    }
+     CloseWindow();
+}
