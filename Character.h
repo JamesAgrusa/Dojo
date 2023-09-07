e488d10d9aad1f297a1fa42f837399d1491b8891
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-06 21:34:05 -0400

    Add files via upload

---
+++ Character.h
@@ -0,0 +1,27 @@
+#include "raylib.h"
+
+class Character
+{
+public:
+    Character();
+    Vector2 getWorldPos() { return worldPos; } // getter
+    void setScreenPos(int windWidth, int winHeight);
+    void tick(float deltaTime);
+
+private:
+    Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
+    Vector2 screenPos{};
+    Vector2 worldPos{};
+    // 1: facing right, -1 : facing left
+    float rightLeft{1.f};
+    // animation variables
+    float runningTime{};
+    int frame{};
+    int maxFrames{6};
+    float updateTime{1.f / 12.f};
+    float speed{4.f};
+    float width{};
+    float height{};
+};
