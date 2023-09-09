63c2406dd76ff6114d295d7b9855388c9c5e2685
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-08 21:46:08 -0400

    Add files via upload

---
+++ mapBoundaries.cpp
@@ -0,0 +1,41 @@
+#include "raylib.h"
+#include "raymath.h"
+#include "Character.h"
+
+int main()
+{
+    const int windowWidth{384};
+    const int windowHeight{384};
+    InitWindow(windowWidth, windowHeight, "Stephen's Top Down");
+
+    Texture2D map = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
+    Vector2 mapPos{0.0, 0.0};
+    const float mapScale{4.0f};
+
+    Character knight;
+    knight.setScreenPos(windowWidth, windowHeight);
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        mapPos = Vector2Scale(knight.getWorldPos(), -1.f);
+
+        // draw the map
+        DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
+        knight.tick(GetFrameTime());
+        // check map bounds
+        if (knight.getWorldPos().x < 0.f ||
+            knight.getWorldPos().y < 0.f ||
+            knight.getWorldPos().x + windowWidth > map.width * mapScale || // scaled by 4
+            knight.getWorldPos().y + windowHeight > map.height * mapScale )
+            {
+                knight.undoMovement();
+            }
+
+        EndDrawing();
+    }
+    CloseWindow();
+}
