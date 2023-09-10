dd3f992a93d73e7bb175d213b5b253168becdb48
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-09 21:44:44 -0400

    Add files via upload

--- Prop.cpp
+++ Prop.cpp
@@ -1,8 +1,15 @@
 #include "Prop.h"
+#include "raymath.h"
 
-Prop::Prop(Vector2 pos):
+Prop::Prop(Vector2 pos, Texture2D tex):
     worldPos(pos),
     texture(tex)
 {
 
+}
+
+void Prop::Render(Vector2 knightPos)
+{
+    Vector2 screenPos{ Vector2Subtract(worldPos, knightPos) };
+    DrawTextureEx(texture, screenPos, 0.f, scale, WHITE);
 }
