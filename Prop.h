06804e29de36a585b582a80d19e510ccecd657f4
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-09 21:44:30 -0400

    Add files via upload

--- Prop.h
+++ Prop.h
@@ -1,11 +1,14 @@
 #include "raylib.h"
 
+#include "raylib.h"
+
 class Prop
 {
-    public:
-        Prop(Vector2 pos, Texture2D tex);
-    private:
+public:
+    Prop(Vector2 pos, Texture2D tex);
+    void Render(Vector2 knightPos);
+private:
     Texture2D texture{};
     Vector2 worldPos{};
-    float scale {4.f};
+    float scale{4.f};
 };
