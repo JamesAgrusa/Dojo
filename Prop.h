81e12004d4bdaef1a2d55ebbef46fd005aae60dc
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-10 22:15:22 -0400

    Add files via upload

--- Prop.h
+++ Prop.h
@@ -1,12 +1,11 @@
 #include "raylib.h"
 
-#include "raylib.h"
-
 class Prop
 {
 public:
     Prop(Vector2 pos, Texture2D tex);
     void Render(Vector2 knightPos);
+    Rectangle getCollisionRec(Vector2 knightPos);
 private:
     Texture2D texture{};
     Vector2 worldPos{};
