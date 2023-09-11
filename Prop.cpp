b7e47913a75cb923ee486275f73db60e7ef813ee
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-10 22:15:39 -0400

    Add files via upload

--- Prop.cpp
+++ Prop.cpp
@@ -13,3 +13,14 @@ void Prop::Render(Vector2 knightPos)
     Vector2 screenPos{ Vector2Subtract(worldPos, knightPos) };
     DrawTextureEx(texture, screenPos, 0.f, scale, WHITE);
 }
+
+Rectangle Prop::getCollisionRec(Vector2 knightPos)
+{
+    Vector2 screenPos{ Vector2Subtract(worldPos, knightPos) };
+    return Rectangle{
+        screenPos.x,
+        screenPos.y,
+        texture.width * scale,
+        texture.height * scale
+    };
+}
