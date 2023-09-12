662d31ca9aa659a6577f07c155e2f71d93ae9ffe
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-11 23:14:40 -0400

    Add files via upload

---
+++ Enemy.h
@@ -0,0 +1,29 @@
+#include "raylib.h"
+
+class Enemy
+{
+public:
+    Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture);
+    Vector2 getWorldPos() { return worldPos; }
+    void tick(float deltaTime);
+    void undoMovement();
+    Rectangle getCollisionRec();
+private:
+    Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
+    Vector2 screenPos{};
+    Vector2 worldPos{};
+    Vector2 worldPosLastFrame{};
+    // 1 : facing right, -1 : facing left
+    float rightLeft{1.f};
+    // animation variables
+    float runningTime{};
+    int frame{};
+    int maxFrames{6};
+    float updateTime{1.f / 12.f};
+    float speed{4.f};
+    float width{};
+    float height{};
+    float scale{4.0f};
+};
