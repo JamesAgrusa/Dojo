3f9f1b3b7f664396772415adbc26c71ff1ce01f5
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-11 23:14:24 -0400

    Add files via upload

---
+++ Enemy.cpp
@@ -0,0 +1,46 @@
+#include "Enemy.h"
+
+Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture) :
+    worldPos(pos),
+    texture(idle_texture),
+    idle(idle_texture),
+    run(run_texture)
+{
+    width = texture.width / maxFrames;
+    height = texture.height;
+}
+
+void Enemy::tick(float deltaTime)
+{
+    worldPosLastFrame = worldPos;
+
+    // update animation frame
+    runningTime += deltaTime;
+    if (runningTime >= updateTime)
+    {
+        frame++;
+        runningTime = 0.f;
+        if (frame > maxFrames)
+            frame = 0;
+    }
+
+    // draw the character
+    Rectangle source{frame * width, 0.f, rightLeft * width, height};
+    Rectangle dest{screenPos.x, screenPos.y, scale * width, scale * height};
+    DrawTexturePro(texture, source, dest, Vector2{}, 0.f, WHITE);
+}
+
+void Enemy::undoMovement()
+{
+    worldPos = worldPosLastFrame;
+}
+
+Rectangle Enemy::getCollisionRec()
+{
+    return Rectangle{
+        screenPos.x,
+        screenPos.y,
+        width * scale,
+        height * scale
+    };
+}
