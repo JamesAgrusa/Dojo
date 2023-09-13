2052774940b5e3a304b39f4117d27f0e557ff931
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-13 19:52:40 -0400

    Add files via upload

--- Enemy.h
+++ Enemy.h
@@ -1,6 +1,7 @@
 #include "raylib.h"
+#include "BaseCharacter.h"
 
-class Enemy
+class Enemy : public BaseCharacter
 {
 public:
     Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture);
