a6151206087c91eed77c5238613fbff5aa9ef80f
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-13 21:05:33 -0400

    Add files via upload

--- BaseCharacter.cpp
+++ BaseCharacter.cpp
@@ -5,3 +5,17 @@ BaseCharacter::BaseCharacter()
     
 }
 
+void BaseCharacter::undoMovement()
+{
+    worldPos = worldPosLastFrame;
+}
+
+Rectangle BaseCharacter::getCollisionRec()
+{
+    return Rectangle{
+        screenPos.x,
+        screenPos.y,
+        width * scale,
+        height * scale
+    };
+}
