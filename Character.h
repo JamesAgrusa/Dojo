19ca98e82caa146dcbdfb3fe56f0e7ab9b7a27ee
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-16 23:25:26 -0400

    Add files via upload

--- Character.h
+++ Character.h
@@ -1,3 +1,6 @@
+#ifndef CHARACTER_H 
+#define CHARACTER_H 
+
 #include "raylib.h"
 #include "BaseCharacter.h"
 
@@ -8,3 +11,5 @@ public:
      virtual void tick(float deltaTime) override;
 private:
 };
+
+#endif
