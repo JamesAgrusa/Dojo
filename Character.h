a06ab12274079a53ec2438491f66d06f6f6d56a2
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-17 18:08:44 -0400

    Add files via upload

--- Character.h
+++ Character.h
@@ -9,6 +9,7 @@ class Character : public BaseCharacter
 public:
     Character(int winWidth, int winHeight);
      virtual void tick(float deltaTime) override;
+     Vector2 getScreenPos() { return screenPos; }
 private:
 };
 
