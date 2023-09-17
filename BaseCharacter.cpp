62ed5e6ee232fadb21e856dbbb4689f55391555c
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-16 23:03:15 -0400

    Add files via upload

--- BaseCharacter.cpp
+++ BaseCharacter.cpp
@@ -19,3 +19,23 @@ Rectangle BaseCharacter::getCollisionRec()
         height * scale
     };
 }
+
+void BaseCharacter::tick(float deltaTime)
+{
+    worldPosLastFrame = worldPos;
+
+     // update animation frame
+    runningTime += deltaTime;
+    if (runningTime >= updateTime)
+    {
+        frame++;
+        runningTime = 0.f;
+        if (frame > maxFrames)
+            frame = 0;
+    }
+
+    // draw the character
+    Rectangle source{frame * width, 0.f, rightLeft * width, height};
+    Rectangle dest{screenPos.x, screenPos.y, scale * width, scale * height};
+    DrawTexturePro(texture, source, dest, Vector2{}, 0.f, WHITE);
+}
