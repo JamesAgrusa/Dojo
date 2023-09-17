98b0bb10c8686214cd865d30e79776ad40b91908
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-16 23:03:04 -0400

    Add files via upload

--- Character.cpp
+++ Character.cpp
@@ -13,7 +13,7 @@ Character::Character(int winWidth, int winHeight)
 
 void Character::tick(float deltaTime)
 {
-    worldPosLastFrame = worldPos;
+    BaseCharacter::tick(deltaTime);
 
     Vector2 direction{};
     if (IsKeyDown(KEY_A))
@@ -35,20 +35,5 @@ void Character::tick(float deltaTime)
     else
     {
         texture = idle;
-    }
-
-    // update animation frame
-    runningTime += deltaTime;
-    if (runningTime >= updateTime)
-    {
-        frame++;
-        runningTime = 0.f;
-        if (frame > maxFrames)
-            frame = 0;
-    }
-
-    // draw the character
-    Rectangle source{frame * width, 0.f, rightLeft * width, height};
-    Rectangle dest{screenPos.x, screenPos.y, scale * width, scale * height};
-    DrawTexturePro(texture, source, dest, Vector2{}, 0.f, WHITE);
+    }    
 }
