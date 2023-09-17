42be805379c49f9837d5653d1ce8a21367648df4
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-16 23:25:37 -0400

    Add files via upload

--- Enemy.h
+++ Enemy.h
@@ -1,10 +1,14 @@
 #include "raylib.h"
 #include "BaseCharacter.h"
+#include "Character.h"
 
 class Enemy : public BaseCharacter
 {
 public:
     Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture);
-   virtual void tick(float deltaTime) override;
+    virtual void tick(float deltaTime) override;
+    void setTarget(Character* character) { target = character;}
+
 private:
+    Character* target;
 };
