8cff4623cc5241871b8ddb5a621e1ecd1715d53b
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-16 23:02:53 -0400

    Add files via upload

--- Enemy.cpp
+++ Enemy.cpp
@@ -12,20 +12,5 @@ Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture)
 
 void Enemy::tick(float deltaTime)
 {
-    worldPosLastFrame = worldPos;
-
-    // update animation frame
-    runningTime += deltaTime;
-    if (runningTime >= updateTime)
-    {
-        frame++;
-        runningTime = 0.f;
-        if (frame > maxFrames)
-            frame = 0;
-    }
-
-    // draw the character
-    Rectangle source{frame * width, 0.f, rightLeft * width, height};
-    Rectangle dest{screenPos.x, screenPos.y, scale * width, scale * height};
-    DrawTexturePro(texture, source, dest, Vector2{}, 0.f, WHITE);
+    BaseCharacter::tick(deltaTime);
 }
