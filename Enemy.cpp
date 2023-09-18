cd847f5df0f5fea59d85c38d2d744028a176750c
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-17 21:27:04 -0400

    Add files via upload

--- Enemy.cpp
+++ Enemy.cpp
@@ -15,14 +15,13 @@ Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture)
 void Enemy::tick(float deltaTime)
 {
     // get toTarget
-    Vector2 toTarget = Vector2Subtract(target->getScreenPos(), screenPos);
-    // normalize to Target
-    toTarget = Vector2Normalize(toTarget);
-    // multiply toTarget by speed
-    toTarget = Vector2Scale(toTarget, speed);
-     // move the enemy
-     worldPos = Vector2Add(worldPos, toTarget);
-    screenPos = Vector2Subtract(worldPos, target->getWorldPos());
+    velocity = Vector2Subtract(target->getScreenPos(), getScreenPos());
     BaseCharacter::tick(deltaTime);
     
 }
+
+Vector2 Enemy::getScreenPos()
+{
+    return Vector2Subtract(worldPos, target->getWorldPos());
+}
+
