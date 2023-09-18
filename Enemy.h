602c7e7026cc6a576cbdb72da25e284d9f8a9221
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-17 21:26:59 -0400

    Add files via upload

--- Enemy.h
+++ Enemy.h
@@ -8,6 +8,7 @@ public:
     Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture);
     virtual void tick(float deltaTime) override;
     void setTarget(Character* character) { target = character;}
+    virtual Vector2 getScreenPos() override;
 
 private:
     Character* target;
