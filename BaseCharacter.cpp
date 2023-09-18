694b8185c3126fc782fc1d0ceb706c1d0bb4032a
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-17 21:26:18 -0400

    Add files via upload

--- BaseCharacter.cpp
+++ BaseCharacter.cpp
@@ -1,4 +1,5 @@
 #include "BaseCharacter.h"
+#include "raymath.h"
 
 BaseCharacter::BaseCharacter()
 {
@@ -13,8 +14,8 @@ void BaseCharacter::undoMovement()
 Rectangle BaseCharacter::getCollisionRec()
 {
     return Rectangle{
-        screenPos.x,
-        screenPos.y,
+        getScreenPos().x,
+        getScreenPos().y,
         width * scale,
         height * scale
     };
@@ -34,8 +35,22 @@ void BaseCharacter::tick(float deltaTime)
             frame = 0;
     }
 
+    if (Vector2Length(velocity) != 0.0)
+    {
+        // set worldPos = worldPos + direction
+
+        worldPos = Vector2Add(worldPos, Vector2Scale(Vector2Normalize(velocity), speed));
+        velocity.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+        texture = run;
+    }
+    else
+    {
+        texture = idle;
+    }  
+    velocity = {};  
+
     // draw the character
     Rectangle source{frame * width, 0.f, rightLeft * width, height};
-    Rectangle dest{screenPos.x, screenPos.y, scale * width, scale * height};
+    Rectangle dest{getScreenPos().x, getScreenPos().y, scale * width, scale * height};
     DrawTexturePro(texture, source, dest, Vector2{}, 0.f, WHITE);
 }
