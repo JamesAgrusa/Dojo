e608e347c26970e91fa38971e6fbc25886a8b2ad
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-17 21:26:22 -0400

    Add files via upload

--- BaseCharacter.h
+++ BaseCharacter.h
@@ -10,11 +10,11 @@ public:
     void undoMovement();
     Rectangle getCollisionRec();
     virtual void tick(float deltaTime);
+    virtual Vector2 getScreenPos()= 0; // pure virtual function 'abstract class'
 protected:
     Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
     Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
     Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
-    Vector2 screenPos{};
     Vector2 worldPos{};
     Vector2 worldPosLastFrame{};
     // 1 : facing right, -1 : facing left
@@ -28,6 +28,7 @@ protected:
     float width{};
     float height{};
     float scale{4.0f};
+    Vector2 velocity{};
 
 private:
 
