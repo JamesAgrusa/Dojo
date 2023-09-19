f536c67d9c623f7e8346cfa7b775e1aa2b8c8709
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-18 23:01:18 -0400

    Add files via upload

--- Character.h
+++ Character.h
@@ -13,6 +13,8 @@ public:
 private:
     int windowWidth{};
     int windowHeight{};
+    Texture2D weapon{LoadTexture("characters/weapon_sword.png")};
+    Rectangle weaponCollisionRec{};
 };
 
 #endif
