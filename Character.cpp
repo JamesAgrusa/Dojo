0e7f7e1b63e4a398ecb2324fef1f9adc9e0816a9
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-18 23:01:13 -0400

    Add files via upload

--- Character.cpp
+++ Character.cpp
@@ -1,39 +1,75 @@
 #include "Character.h"
 #include "raymath.h"
 
-Character::Character(int winWidth, int winHeight)
+Character::Character(int winWidth, int winHeight) :
+    windowWidth(winWidth),
+    windowHeight(winHeight)   
 {
     width = texture.width / maxFrames;
     height = texture.height;
+}
+    
 
-    screenPos = {static_cast<float>(winWidth) / 2.0f - scale * (0.5f * width),
-                 static_cast<float>(winHeight) / 2.0f - scale * (0.5f * height)
+Vector2 Character::getScreenPos()
+{
+    return Vector2{
+        static_cast<float>(windowWidth) / 2.0f - scale * (0.5f * width),
+        static_cast<float>(windowHeight) / 2.0f - scale * (0.5f * height)
     };
 }
 
+
 void Character::tick(float deltaTime)
 {
+    
+    if (IsKeyDown(KEY_A))
+        velocity.x -= 1.0;
+    if (IsKeyDown(KEY_D))
+       velocity.x += 1.0;
+    if (IsKeyDown(KEY_W))
+        velocity.y -= 1.0;
+    if (IsKeyDown(KEY_S))
+        velocity.y += 1.0;
     BaseCharacter::tick(deltaTime);
 
-    Vector2 direction{};
-    if (IsKeyDown(KEY_A))
-        direction.x -= 1.0;
-    if (IsKeyDown(KEY_D))
-        direction.x += 1.0;
-    if (IsKeyDown(KEY_W))
-        direction.y -= 1.0;
-    if (IsKeyDown(KEY_S))
-        direction.y += 1.0;
-    if (Vector2Length(direction) != 0.0)
+    Vector2 origin{};
+    Vector2 offset{};
+    float rotation{};
+    if(rightLeft > 0.f)
     {
-        // set worldPos = worldPos + direction
-
-        worldPos = Vector2Add(worldPos, Vector2Scale(Vector2Normalize(direction), speed));
-        direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
-        texture = run;
+        origin = {0.f, weapon.height * scale};
+        offset = {35.f, 55.f};
+        weaponCollisionRec = {
+        getScreenPos().x + offset.x,
+        getScreenPos().y + offset.y - weapon.height * scale,
+        weapon.width * scale,
+        weapon.height * scale
+       };
+       rotation = {35.f};
     }
     else
     {
-        texture = idle;
-    }    
+       origin = {weapon.width * scale, weapon.height * scale }; 
+       offset = {25.f, 55.f};
+      weaponCollisionRec = {
+        getScreenPos().x + offset.x - weapon.width * scale,
+        getScreenPos().y + offset.y - weapon.height * scale,
+        weapon.width * scale,
+        weapon.height * scale
+       };
+        rotation = {-35};
+    }
+
+    // draw the sword
+    Rectangle source{0.f, 0.f, static_cast<float>(weapon.width) * rightLeft, static_cast<float>(weapon.height)};
+    Rectangle dest{getScreenPos().x + offset.x, getScreenPos().y + offset.y, weapon.width * scale, weapon.height * scale};
+    DrawTexturePro(weapon, source, dest, origin, rotation, WHITE);
+
+    DrawRectangleLines(
+        weaponCollisionRec.x,
+        weaponCollisionRec.y,
+        weaponCollisionRec.width,
+        weaponCollisionRec.height,
+        RED
+    );
 }
