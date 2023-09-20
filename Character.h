eea49dce98eda4c54fb305c2a99f8ba4b309880f
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-19 22:18:54 -0400

    Add files via upload

--- Character.h
+++ Character.h
@@ -11,11 +11,14 @@ public:
      virtual void tick(float deltaTime) override;
      virtual Vector2 getScreenPos() override;
      Rectangle getWeaponCollisionRec() { return weaponCollisionRec; }
+     float getHealth() const { return health; };
+     void takeDamage(float damage);
 private:
     int windowWidth{};
     int windowHeight{};
     Texture2D weapon{LoadTexture("characters/weapon_sword.png")};
     Rectangle weaponCollisionRec{};
+    float health{100.f};
 };
 
 #endif
