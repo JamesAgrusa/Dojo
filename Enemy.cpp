bece61f69500147620d40bab0dbbdf6ec0963131
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-19 22:19:05 -0400

    Add files via upload

--- Enemy.cpp
+++ Enemy.cpp
@@ -18,6 +18,11 @@ void Enemy::tick(float deltaTime)
     // to get target
     velocity = Vector2Subtract(target->getScreenPos(), getScreenPos());
     BaseCharacter::tick(deltaTime);
+
+    if (CheckCollisionRecs(target->getCollisionRec(), getCollisionRec()))
+    {
+        target->takeDamage(damagePerSec * deltaTime);
+    }
     
 }
 
