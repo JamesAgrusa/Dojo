d5de989660b47befbc7538625f92f9d80f9d79a0
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-19 22:11:00 -0400

    Add files via upload

--- Character.h
+++ Character.h
@@ -10,6 +10,7 @@ public:
     Character(int winWidth, int winHeight);
      virtual void tick(float deltaTime) override;
      virtual Vector2 getScreenPos() override;
+     Rectangle getWeaponCollisionRec() { return weaponCollisionRec; }
 private:
     int windowWidth{};
     int windowHeight{};
