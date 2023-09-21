18e020d6893e717078e7c7d8ff98784b1cd28b92
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-20 22:58:13 -0400

    Add files via upload

--- Character.h
+++ Character.h
@@ -1,5 +1,6 @@
-#ifndef CHARACTER_H 
-#define CHARACTER_H 
+
+#ifndef CHARACTER_H
+#define CHARACTER_H
 
 #include "raylib.h"
 #include "BaseCharacter.h"
@@ -8,11 +9,11 @@ class Character : public BaseCharacter
 {
 public:
     Character(int winWidth, int winHeight);
-     virtual void tick(float deltaTime) override;
-     virtual Vector2 getScreenPos() override;
-     Rectangle getWeaponCollisionRec() { return weaponCollisionRec; }
-     float getHealth() const { return health; };
-     void takeDamage(float damage);
+    virtual void tick(float deltaTime) override;
+    virtual Vector2 getScreenPos() override;
+    Rectangle getWeaponCollisionRec() { return weaponCollisionRec; }
+    float getHealth() const { return health; }
+    void takeDamage(float damage);
 private:
     int windowWidth{};
     int windowHeight{};
