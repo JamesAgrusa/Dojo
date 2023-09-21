42cdb25355920e709e3ab52d0481b4403c19bcd4
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-20 22:58:01 -0400

    Add files via upload

---
+++ DisplayHealth.cpp
@@ -0,0 +1,93 @@
+#include "raylib.h"
+#include "raymath.h"
+#include "Character.h"
+#include "Prop.h"
+#include "Enemy.h"
+#include <string>
+
+int main()
+{
+    const int windowWidth{384};
+    const int windowHeight{384};
+    InitWindow(windowWidth, windowHeight, "Stephen's Top Down");
+
+    Texture2D map = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
+    Vector2 mapPos{0.0, 0.0};
+    const float mapScale{4.0f};
+
+    Character knight{windowWidth, windowHeight};
+
+    Prop props[2]{
+        Prop{Vector2{600.f, 300.f}, LoadTexture("nature_tileset/Rock.png")},
+        Prop{Vector2{400.f, 500.f}, LoadTexture("nature_tileset/Log.png")}
+    };
+
+    Enemy goblin{
+        Vector2{},
+        LoadTexture("characters/goblin_idle_spritesheet.png"),
+        LoadTexture("characters/goblin_run_spritesheet.png")
+    };
+    goblin.setTarget(&knight);
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        mapPos = Vector2Scale(knight.getWorldPos(), -1.f);
+
+        // draw the map
+        DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
+
+        // draw the props
+        for (auto prop : props)
+        {
+            prop.Render(knight.getWorldPos());
+        }
+
+        if (!knight.getAlive()) // Character is not alive
+        {
+            DrawText("Game Over!", 55.f, 45.f, 40, RED);
+            EndDrawing();
+            continue;
+        }
+        else // Character is alive
+        {
+            std::string knightsHealth = "Health: ";
+            knightsHealth.append(std::to_string(knight.getHealth()), 0, 5);
+            DrawText(knightsHealth.c_str(), 55.f, 45.f, 40, RED);
+        }
+
+        knight.tick(GetFrameTime());
+        // check map bounds
+        if (knight.getWorldPos().x < 0.f ||
+            knight.getWorldPos().y < 0.f ||
+            knight.getWorldPos().x + windowWidth > map.width * mapScale ||
+            knight.getWorldPos().y + windowHeight > map.height * mapScale)
+        {
+            knight.undoMovement();
+        }
+        // check prop collisions
+        for (auto prop : props)
+        {
+            if (CheckCollisionRecs(prop.getCollisionRec(knight.getWorldPos()), knight.getCollisionRec()))
+            {
+                knight.undoMovement();
+            }
+        }
+
+        goblin.tick(GetFrameTime());
+
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        {
+            if (CheckCollisionRecs(goblin.getCollisionRec(), knight.getWeaponCollisionRec()))
+            {
+                goblin.setAlive(false);
+            }
+        }
+
+        EndDrawing();
+    }
+    CloseWindow();
+}
