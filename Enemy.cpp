c2b45a0f7a777481d00c867b62cf97416836cb41
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-20 23:11:41 -0400

    Add files via upload

--- Enemy.cpp
+++ Enemy.cpp
@@ -15,15 +15,15 @@ Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture)
 void Enemy::tick(float deltaTime)
 {
     if (!getAlive()) return;
-    // to get target
+    // get toTarget
     velocity = Vector2Subtract(target->getScreenPos(), getScreenPos());
+    if (Vector2Length(velocity) < radius) velocity = {};
     BaseCharacter::tick(deltaTime);
 
     if (CheckCollisionRecs(target->getCollisionRec(), getCollisionRec()))
     {
         target->takeDamage(damagePerSec * deltaTime);
     }
-    
 }
 
 Vector2 Enemy::getScreenPos()
