bda2f33e6a2512acc226bb84a89bee5f9370c65d
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-20 22:58:08 -0400

    Add files via upload

--- Character.cpp
+++ Character.cpp
@@ -3,12 +3,11 @@
 
 Character::Character(int winWidth, int winHeight) :
     windowWidth(winWidth),
-    windowHeight(winHeight)   
+    windowHeight(winHeight)
 {
     width = texture.width / maxFrames;
     height = texture.height;
 }
-    
 
 Vector2 Character::getScreenPos()
 {
@@ -16,8 +15,7 @@ Vector2 Character::getScreenPos()
         static_cast<float>(windowWidth) / 2.0f - scale * (0.5f * width),
         static_cast<float>(windowHeight) / 2.0f - scale * (0.5f * height)
     };
-}
-
+} 
 
 void Character::tick(float deltaTime)
 {
@@ -26,7 +24,7 @@ void Character::tick(float deltaTime)
     if (IsKeyDown(KEY_A))
         velocity.x -= 1.0;
     if (IsKeyDown(KEY_D))
-       velocity.x += 1.0;
+        velocity.x += 1.0;
     if (IsKeyDown(KEY_W))
         velocity.y -= 1.0;
     if (IsKeyDown(KEY_S))
@@ -36,28 +34,28 @@ void Character::tick(float deltaTime)
     Vector2 origin{};
     Vector2 offset{};
     float rotation{};
-    if(rightLeft > 0.f)
+    if (rightLeft > 0.f)
     {
         origin = {0.f, weapon.height * scale};
         offset = {35.f, 55.f};
         weaponCollisionRec = {
-        getScreenPos().x + offset.x,
-        getScreenPos().y + offset.y - weapon.height * scale,
-        weapon.width * scale,
-        weapon.height * scale
-       };
-       rotation = IsMouseButtonDown(MOUSE_LEFT_BUTTON) ? 35.f : 0.f; // terenary operator
+            getScreenPos().x + offset.x,
+            getScreenPos().y + offset.y - weapon.height * scale,
+            weapon.width * scale,
+            weapon.height * scale
+        };
+        rotation = IsMouseButtonDown(MOUSE_LEFT_BUTTON) ? 35.f : 0.f;
     }
     else
     {
-       origin = {weapon.width * scale, weapon.height * scale }; 
-       offset = {25.f, 55.f};
-      weaponCollisionRec = {
-        getScreenPos().x + offset.x - weapon.width * scale,
-        getScreenPos().y + offset.y - weapon.height * scale,
-        weapon.width * scale,
-        weapon.height * scale
-       };
+        origin = {weapon.width * scale, weapon.height * scale};
+        offset = {25.f, 55.f};
+        weaponCollisionRec = {
+            getScreenPos().x + offset.x - weapon.width * scale,
+            getScreenPos().y + offset.y - weapon.height * scale,
+            weapon.width * scale,
+            weapon.height * scale
+        };
         rotation = IsMouseButtonDown(MOUSE_LEFT_BUTTON) ? -35.f : 0.f;
     }
 
@@ -74,10 +72,11 @@ void Character::tick(float deltaTime)
         RED
     );
 }
+
 void Character::takeDamage(float damage)
 {
-    health -+ damage;
-    if(health <= 0.f)
+    health -= damage;
+    if (health <= 0.f)
     {
         setAlive(false);
     }
