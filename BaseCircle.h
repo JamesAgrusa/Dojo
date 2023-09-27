c6a4a3e0d2483b82c1167a3e112853470b47940e
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-09-26 22:40:09 -0400

    Add files via upload

---
+++ BaseCircle.h
@@ -0,0 +1,14 @@
+#ifndef BASE_CIRCLE_H
+#define BASE_CIRCLE_H
+#include "raylib.h"
+
+class BaseCircle
+{
+    public:
+    BaseCircle();
+    virtual void tick(float deltaTime);
+    private:
+
+};
+
+#endif
