5597b4a2c813f8060f87581fcd633a1b82fe4828
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-10-09 00:10:43 -0400

    Add files via upload

--- TopDownGame.cpp
+++ TopDownGame.cpp
@@ -3,19 +3,31 @@
 
 int main()
 {
-    const int windowWidth{384};
-    const int windowHeight{384};
+    const int windowWidth{950};
+    const int windowHeight{950};
     InitWindow(windowWidth, windowHeight, "Another Top Down!");
 
     Texture2D map = LoadTexture("map_tileset/TopDownMap.png");
     Vector2 mapPos{0.0, 0.0};
-    float speed{4.0};
+    float speed{10.0};
+    Texture2D knight_idle = LoadTexture("Textures/knight2.png");
+    Texture2D knight_run = LoadTexture("Textures/knight2.png");
+
 
     Texture2D knight = LoadTexture("Textures/knight2.png");
     Vector2 knightPos{
         (float)windowWidth/2.0f - 4.0f * (0.5f * (float)knight.width/6.0f),
         (float)windowHeight/2.0f - 4.0f * (0.5f * (float)knight.height)
     };
+
+    //1: facing righ, -1 : facing left
+    float rightLeft{1.f};
+    // animation variables
+    float runningTime{};
+    int frame{};
+    const int maxFrames{6};
+    const float updateTime{1.f/12.f}; // will update 12 times per second
+
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -33,6 +45,23 @@ int main()
             // set mapPos = mapPos - direction
             
             mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+            direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f; // equivilant to an if else statement. 'ternairy opporator'
+            knight = knight_run;
+        }
+         else
+        {
+            knight = knight_idle;
+        }
+        // draw the map
+        DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
+
+        // update animation frame
+        runningTime += GetFrameTime();
+        if (runningTime >= updateTime)
+        {
+            frame++;
+            runningTime = 0.f;
+            if(frame > maxFrames) frame = 0;
         }
         
         // draw the map
