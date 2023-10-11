b313d4e117e8467a12fd3cd5cddcd4e3679fa74a
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-10-10 21:11:39 -0400

    Add files via upload

---
+++ BaseHero.cpp
@@ -0,0 +1,44 @@
+#include "BaseHero.h"
+#include "raymath.h"
+
+BaseHero::BaseHero()
+{
+
+}
+
+void BaseHero::undowMovement()
+{
+    WorldPos = worldPosLastFrame;
+}
+
+void BaseHero::tick(float deltaTime)
+{
+    worldPosLastFrame = worldPos;
+
+    // update animation frame
+    runningTime += deltaTime;
+    if(runningTime >= updateTime)
+    {
+        frame++;
+        runningTime = 0.f;
+        if(frame > maxFrames)
+        frame = 0;
+    }
+
+    if (Vector2Length(velocity) != 0.0)
+    {
+        worldPos = Vector2Add(worldPos, Vector2Scale(Vector2Normalize(velocity), speed));
+        velocity.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+        texture = run;
+    }
+    else
+    {
+        texture = idle;
+    }
+    velocity = {};
+    
+    // draw the character
+    Rectangle source{frame * width, 0.f, rightLeft * width, height};
+    Rectangle dest{getScreenPos().x, getScreenPos().y, scale * width, scale * height};
+    DrawTexturePro(texture, source, dest, Vector2{}, 0.f, WHITE);
+}
