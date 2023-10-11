85c2baa9880bc9f69d4986276cfdb56d355c9d69
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-10-10 21:11:46 -0400

    Add files via upload

---
+++ BaseHero.h
@@ -0,0 +1,36 @@
+#ifndef BASE_HERO_H
+#define BASE_HERO_H
+#include "raylib.h"
+
+class BaseHero
+{
+    public:
+    BaseHero();
+    Vector2 getWorldPos() { return worldPos; }
+    void undowMovement();
+    virtual void tick(float deltaTime);
+    virtual void Vector2 getScreenPos() = 0;
+
+    protected:
+    Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
+    Vector2 worldPos{};
+    Vector2 worldPosLastFrame{};
+    // 1 : facing right, -1 : facing left
+    float rightLeft{1.f};
+    // animation variables
+    float runningTime{};
+    int frame{};
+    int maxFrames{6};
+    float updateTime{1.f / 12.f};
+    float speed{4.f};
+    float width{};
+    float height{};
+    float scale{4.0f};
+    Vector2 velocity{};
+
+    private:
+}
+
+#endif
