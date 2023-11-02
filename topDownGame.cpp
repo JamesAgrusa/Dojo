cf6d30ac393da52622e5fef3094190b9ce027a8d
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-11-01 23:11:26 -0400

    Add files via upload

--- topDownGame.cpp
+++ topDownGame.cpp
@@ -11,236 +11,236 @@ typedef enum GameScreen { LOGO = 0, TITLE, GAMEPLAY, ENDING } GameScreen;
 //------------------------------------------------------------------------------------
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+// Initialization
+//--------------------------------------------------------------------------------------
+const int screenWidth = 800;
+const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "Soldier Destruction");
+InitWindow(screenWidth, screenHeight, "Soldier Destruction");
 
-    GameScreen currentScreen = LOGO;
+GameScreen currentScreen = LOGO;
 
-    // TODO: Initialize all required variables and load all required data here!
+// TODO: Initialize all required variables and load all required data here!
 
-    int framesCounter = 0;          // Useful to count frames
+int framesCounter = 0;          // Useful to count frames
 
-    SetTargetFPS(60);               // Set desired framerate (frames-per-second)
-    //--------------------------------------------------------------------------------------
+SetTargetFPS(60);               // Set desired framerate (frames-per-second)
+//--------------------------------------------------------------------------------------
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+// Main game loop
+while (!WindowShouldClose())    // Detect window close button or ESC key
+{
+    // Update
+    //----------------------------------------------------------------------------------
+    switch(currentScreen)
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        switch(currentScreen)
-        {
             case LOGO:
             {
-                // TODO: Update LOGO screen variables here!
+            // TODO: Update LOGO screen variables here!
 
                 framesCounter++;    // Count frames
 
                 // Wait for 6 seconds (360 frames) before jumping to TITLE screen
-                if (framesCounter > 360)
+                if (framesCounter > 60)
                 {
-                    currentScreen = TITLE;
+                currentScreen = TITLE;
                 }
+            } break;
+        case TITLE:
+        {
+            // TODO: Update TITLE screen variables here!
+
+            // Press enter to change to GAMEPLAY screen
+            if (IsMouseButtonDown(MOUSE_RIGHT_BUTTON) || IsGestureDetected(GESTURE_TAP))
+            {
+                currentScreen = GAMEPLAY;
+            }
+        } break;
+        case GAMEPLAY:
+        {
+            // TODO: Update GAMEPLAY screen variables here!
+
+            // Press enter to change to ENDING screen
+            if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+            {
+                currentScreen = ENDING;
+            }
+        } break;
+        case ENDING:
+        {
+            // TODO: Update ENDING screen variables here!
+
+            // Press enter to return to TITLE screen
+            if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+            {
+                currentScreen = TITLE;
+            }
+        } break;
+        default: break;
+    }
+    //----------------------------------------------------------------------------------
+
+    // Draw
+    //----------------------------------------------------------------------------------
+    BeginDrawing();
+
+        ClearBackground(ORANGE);
+
+        switch(currentScreen)
+        {
+            case LOGO:
+            {
+            // TODO: Draw LOGO screen here!
+                DrawText("UnrealityArts", 230, 20, 40, BLACK);
+                DrawText("The Second UnrealityArts Game", 150, 220, 30, BLACK);
+
             } break;
             case TITLE:
             {
-                // TODO: Update TITLE screen variables here!
+                // TODO: Draw TITLE screen here!
+                DrawRectangle(0, 0, screenWidth, screenHeight, DARKBLUE);
+                DrawText("Soldier Destruction", 150, 20, 50, WHITE);
+                DrawText("Right click to Start", 220, 220, 30, WHITE);
 
-                // Press enter to change to GAMEPLAY screen
-                if (IsMouseButtonDown(MOUSE_RIGHT_BUTTON) || IsGestureDetected(GESTURE_TAP))
-                {
-                    currentScreen = GAMEPLAY;
-                }
             } break;
             case GAMEPLAY:
             {
-                // TODO: Update GAMEPLAY screen variables here!
-
-                // Press enter to change to ENDING screen
-                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
                 {
-                    currentScreen = ENDING;
+                    const int windowWidth{1100};
+                    const int windowHeight{900};
+                    InitWindow(windowWidth, windowHeight, "Soldier Destruction");
+
+                    Texture2D map = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
+                    Vector2 mapPos{0.0, 0.0};
+                    float speed{4.0};
+
+                    Texture2D axe{LoadTexture("Weapons/Lance2/Lance.png")};
+
+
+                    Texture2D soldier_walk = LoadTexture("newcharacters/soldier_walk.png");
+                    Texture2D soldier = LoadTexture("newcharacters/soldier_walk.png");
+                    Vector2 soldierPos{
+                    (float)windowWidth/2.0f - 4.0f * (0.5f * (float)soldier.width/4.0f),
+                    (float)windowHeight/2.0f - 4.0f * (0.5f * (float)soldier.height)
+                    };
+
+                    // 1 : facing right, -1 : facing left
+                    float rightLeft{1.f};
+                    // animation variables
+                    float runningTime{};
+                    int frame{};
+                    const int maxFrames{4};
+                    const float updateTime{1.f/10.f};
+                    float scale{4.0f};
+
+                    SetTargetFPS(60);
+                    while (!WindowShouldClose())
+                        {
+                            BeginDrawing();
+                            ClearBackground(WHITE);
+
+                            // moving the character
+        
+                            Vector2 direction{};
+                            if (IsKeyDown(KEY_A)) direction.x -= 1.0;
+                            if (IsKeyDown(KEY_D)) direction.x += 1.0;
+                            if (IsKeyDown(KEY_W)) direction.y -= 1.0;
+                            if (IsKeyDown(KEY_S)) direction.y += 1.0;
+                            if (Vector2Length(direction) != 0.0)
+                            {
+                                // set mapPos = mapPos - direction
+                                
+                                mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+                                direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+                                soldier = soldier_walk;
+                            }
+                            else
+                            {
+                                frame++;
+                                runningTime = 0.f;
+                                if (frame > maxFrames) frame = 4;
+                                soldier = soldier_walk;
+                            }
+
+        
+        
+                            // draw the map
+                            DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
+
+                            // update animation frame
+                            runningTime += GetFrameTime();
+                            if (runningTime >= updateTime)
+                            {
+                                frame++;
+                                runningTime = 0.f;
+                                if (frame > maxFrames) frame = 0;
+                            }
+
+                            // draw the character
+                            
+                            Rectangle source{frame * (float)soldier.width/4.f, 0.f, rightLeft * (float)soldier.width/4.f, (float)soldier.height};
+                            Rectangle dest{soldierPos.x, soldierPos.y, 3.0f * (float)soldier.width/4.0f, 3.0f * (float)soldier.height};
+                            DrawTexturePro(soldier, source, dest, Vector2{}, 0.f, WHITE);
+
+                            // swing the axe
+
+                            Vector2 origin{};
+                            Vector2 offset{};
+                            float rotation{};
+                            if (rightLeft > 0.f)
+                            {
+                                origin = {axe.width * scale, axe.height  * scale};
+                                offset = {40.f, 60.f};
+                                offset.x = 25.f;
+                                offset.y = 75.f;
+                                
+                                rotation = IsMouseButtonDown(MOUSE_LEFT_BUTTON) ? 85.f : 60.f;
+                            }
+                            else
+                            {
+                                origin = {axe.width * scale, axe.height * scale};
+                                offset = {50.f, 100.f};
+                                offset.x = 40.f;
+                                offset.y = 50.f;
+                                
+                                rotation = IsMouseButtonDown(MOUSE_LEFT_BUTTON) ? -85.f : -60.f;
+                            }
+
+        
+                            // draw the axe 
+
+                            Rectangle source1{0.f, 0.f, static_cast<float>(axe.width) * rightLeft, static_cast<float>(axe.height)};
+                            Rectangle dest1{soldierPos.x + offset.x, soldierPos.y + offset.y, axe.width * scale, axe.height * scale};
+                            DrawTexturePro(axe, source1, dest1, origin, rotation, WHITE);
+
+                            EndDrawing();
+                        }
+                    CloseWindow();
                 }
+
             } break;
             case ENDING:
             {
-                // TODO: Update ENDING screen variables here!
+                // TODO: Draw ENDING screen here!
+                DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
+                DrawText("ENDING SCREEN", 20, 20, 40, DARKBLUE);
+                DrawText("PRESS ENTER or TAP to RETURN to TITLE SCREEN", 120, 220, 20, DARKBLUE);
 
-                // Press enter to return to TITLE screen
-                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
-                {
-                    currentScreen = TITLE;
-                }
             } break;
             default: break;
         }
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-
-            ClearBackground(RAYWHITE);
-
-            switch(currentScreen)
-            {
-                case LOGO:
-                {
-                    // TODO: Draw LOGO screen here!
-                    DrawText("UnrealityArts", 120, 20, 40, BLACK);
-                    DrawText("Welcome To The Game", 120, 220, 20, BLACK);
-
-                } break;
-                case TITLE:
-                {
-                    // TODO: Draw TITLE screen here!
-                    DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
-                    DrawText("Soldier Destruction", 120, 20, 40, YELLOW);
-                    DrawText("Right click to Start", 120, 220, 20, YELLOW);
-
-                } break;
-                case GAMEPLAY:
-                {
-                    {
-    const int windowWidth{1100};
-    const int windowHeight{900};
-    InitWindow(windowWidth, windowHeight, "Soldier Destruction");
-
-    Texture2D map = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
-    Vector2 mapPos{0.0, 0.0};
-    float speed{4.0};
-
-    Texture2D axe{LoadTexture("Weapons/Lance2/Lance.png")};
-    
-    
-    Texture2D soldier_walk = LoadTexture("newcharacters/soldier_walk.png");
-    Texture2D soldier = LoadTexture("newcharacters/soldier_walk.png");
-    Vector2 soldierPos{
-        (float)windowWidth/2.0f - 4.0f * (0.5f * (float)soldier.width/4.0f),
-        (float)windowHeight/2.0f - 4.0f * (0.5f * (float)soldier.height)
-    };
-
-    // 1 : facing right, -1 : facing left
-    float rightLeft{1.f};
-    // animation variables
-    float runningTime{};
-    int frame{};
-    const int maxFrames{4};
-    const float updateTime{1.f/10.f};
-    float scale{4.0f};
-
-    SetTargetFPS(60);
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-        ClearBackground(WHITE);
-
-        // moving the character
-        
-        Vector2 direction{};
-        if (IsKeyDown(KEY_A)) direction.x -= 1.0;
-        if (IsKeyDown(KEY_D)) direction.x += 1.0;
-        if (IsKeyDown(KEY_W)) direction.y -= 1.0;
-        if (IsKeyDown(KEY_S)) direction.y += 1.0;
-        if (Vector2Length(direction) != 0.0)
-        {
-            // set mapPos = mapPos - direction
-            
-            mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
-            direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
-            soldier = soldier_walk;
-        }
-        else
-        {
-            frame++;
-            runningTime = 0.f;
-            if (frame > maxFrames) frame = 4;
-            soldier = soldier_walk;
-        }
-
-       
-        
-        // draw the map
-        DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
-
-        // update animation frame
-        runningTime += GetFrameTime();
-        if (runningTime >= updateTime)
-        {
-            frame++;
-            runningTime = 0.f;
-            if (frame > maxFrames) frame = 0;
-        }
-
-        // draw the character
-       
-        Rectangle source{frame * (float)soldier.width/4.f, 0.f, rightLeft * (float)soldier.width/4.f, (float)soldier.height};
-        Rectangle dest{soldierPos.x, soldierPos.y, 3.0f * (float)soldier.width/4.0f, 3.0f * (float)soldier.height};
-        DrawTexturePro(soldier, source, dest, Vector2{}, 0.f, WHITE);
-
-        // swing the axe
-
-        Vector2 origin{};
-        Vector2 offset{};
-        float rotation{};
-        if (rightLeft > 0.f)
-        {
-            origin = {axe.width * scale, axe.height  * scale};
-            offset = {40.f, 60.f};
-            offset.x = 25.f;
-            offset.y = 75.f;
-            
-            rotation = IsMouseButtonDown(MOUSE_LEFT_BUTTON) ? 85.f : 60.f;
-        }
-        else
-        {
-            origin = {axe.width * scale, axe.height * scale};
-            offset = {50.f, 100.f};
-            offset.x = 40.f;
-            offset.y = 50.f;
-           
-            rotation = IsMouseButtonDown(MOUSE_LEFT_BUTTON) ? -85.f : -60.f;
-        }
-
-        
-        // draw the axe 
-
-        Rectangle source1{0.f, 0.f, static_cast<float>(axe.width) * rightLeft, static_cast<float>(axe.height)};
-        Rectangle dest1{soldierPos.x + offset.x, soldierPos.y + offset.y, axe.width * scale, axe.height * scale};
-        DrawTexturePro(axe, source1, dest1, origin, rotation, WHITE);
 
         EndDrawing();
-    }
-    CloseWindow();
+    //----------------------------------------------------------------------------------
 }
 
-                } break;
-                case ENDING:
-                {
-                    // TODO: Draw ENDING screen here!
-                    DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
-                    DrawText("ENDING SCREEN", 20, 20, 40, DARKBLUE);
-                    DrawText("PRESS ENTER or TAP to RETURN to TITLE SCREEN", 120, 220, 20, DARKBLUE);
+// De-Initialization
+//--------------------------------------------------------------------------------------
 
-                } break;
-                default: break;
-            }
+// TODO: Unload all loaded data (textures, fonts, audio) here!
 
-         EndDrawing();
-        //----------------------------------------------------------------------------------
-    }
+CloseWindow();        // Close window and OpenGL context
+//--------------------------------------------------------------------------------------
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-
-    // TODO: Unload all loaded data (textures, fonts, audio) here!
-
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
-    return 0;
+return 0;
 }
