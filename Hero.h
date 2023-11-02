235056c3578158d97cecae15d67c6b978c35b317
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-11-01 23:40:59 -0400

    Add files via upload

--- Hero.h
+++ Hero.h
@@ -1,38 +1,27 @@
 #include "raylib.h"
 
-
-
 class Hero
 {
-public:
-Hero(int winWidth, int winHeight);
-virtual void tick(float deltaTime);
-virtual Vector2 getScreenPos();
-Vector2 getWorldPos() { return worldPos; }
-void undoMovement();
+    public:
+    Hero(int windowWidth, int windowHeight);
+    void tick(float deltaTime);
+    virtual Vector2 soldierPos();
 
-protected:
-Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
-Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
-Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
-Vector2 worldPos{};
-Vector2 worldPosLastFrame{};
-// 1 : facing right, -1 : facing left
-float rightLeft{1.f};
-// animation variables
-float runningTime{};
-int frame{};
-int maxFrames{6};
-float updateTime{1.f / 12.f};
-float speed{4.f};
-float width{};
-float height{};
-float scale{4.0f};
-Vector2 velocity{};
+    protected:
+    Texture2D soldier{LoadTexture("newcharacters/soldier_walk.png")};
+    Texture2D soldier_walk{LoadTexture("newcharacters/soldier_walk.png")};
+    // 1 : facing right, -1 : facing left
+    float rightLeft{1.f};
+    // animation variables
+    float runningTime{};
+    int frame{};
+    const int maxFrames{4};
+    const float updateTime{1.f/10.f};
+    float scale{4.0f};
+    float speed{4.0};
 
-private:
-int windowWidth{};
-int windowHeight{};
-
+    private:
+    int windoWidth{1100};
+    int windowHeight{900};
+    Texture2D axe{LoadTexture("Weapons/Lance2/Lance.png")};
 };
-
