ece71e7590afad27cd56c05b75600144738fc833
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-11-06 23:42:10 -0500

    Add files via upload

--- Hero.h
+++ Hero.h
@@ -1,33 +1,21 @@
+#ifndef HERO_H
+#define HERO_H
+
 #include "raylib.h"
+#include "BaseHero.h"
 
 class Hero
 {
     public:
     Hero(int windowWidth, int windowHeight);
-    virtual void tick(float deltaTime);
-    virtual Vector2 getScreenPos();
-    Vector2 getWorldPos() { return worldPos; }
+    virtual void tick(float deltaTime) override;
+    virtual Vector2 getScreenPos() override;
     // void undoMovement();
     
-
-    protected:
-    Texture2D soldier{LoadTexture("newcharacters/soldier_walk.png")};
-    Texture2D soldier_walk{LoadTexture("newcharacters/soldier_walk.png")};
-    Vector2 worldPos{};
-    Vector2 worldPosLastFrame{}; 
-    float rightLeft{-1.f};
-    float runningTime{};
-    int frame{};
-    const int maxFrames{4};
-    const float updateTime{1.f/10.f};
-    float scale{4.0f};
-    float speed{4.0};
-    float height {};
-    float width{};
-    
-
     private:
     int windowWidth{1100};
     int windowHeight{900};
     Texture2D axe{LoadTexture("Weapons/Lance2/Lance.png")};
 };
+
+#endif
