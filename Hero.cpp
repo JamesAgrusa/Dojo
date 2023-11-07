e1ab084867e822dea0b3a4124551a4bd37516ff2
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-11-06 23:42:21 -0500

    Add files via upload

--- Hero.cpp
+++ Hero.cpp
@@ -28,36 +28,8 @@ void Hero::tick(float deltaTime)
     if (IsKeyDown(KEY_W)) direction.y += 1.0;
     if (IsKeyDown(KEY_S)) direction.y -= 1.0;
     
-    if (Vector2Length(direction) != 0.0)
-        {
-            // set mapPos = mapPos - direction
-                                
-            worldPos = Vector2Subtract(worldPos, Vector2Scale(Vector2Normalize(direction), speed));
-            direction.x < 0.f ? rightLeft = 1.f : rightLeft = -1.f;
-            soldier = soldier_walk;
-        }
-        else
-        {
-            frame++;
-            runningTime = 0.f;
-            if (frame > maxFrames) frame = 4;
-            soldier = soldier_walk;
-        }
-        
-        runningTime += GetFrameTime();
-        if (runningTime >= updateTime)
-        {
-            frame++;
-            runningTime = 0.f;
-            if (frame > maxFrames) frame = 0;
-        }
-
-    // Draw the character
-
-    Rectangle source{frame * width, 0.f, rightLeft * width, height};
-    Rectangle dest{getScreenPos().x, getScreenPos().y, 3.0f * width, 3.0f * height};
-    DrawTexturePro(soldier, source, dest, Vector2{}, 0.f, WHITE);
-
+    
+   
      Vector2 origin{};
      Vector2 offset{};
      float rotation{};
@@ -66,16 +38,16 @@ void Hero::tick(float deltaTime)
          origin = {axe.width * scale, axe.height  * scale};
          offset = {40.f, 60.f};
          offset.x = 25.f;
-         offset.y = 75.f;
+         offset.y = 95.f;
                                 
-         rotation = IsMouseButtonDown(MOUSE_LEFT_BUTTON) ? 85.f : 60.f;
+         rotation = IsMouseButtonDown(MOUSE_LEFT_BUTTON) ? 85.f : 70.f;
      }
      else
      {
         origin = {axe.width * scale, axe.height * scale};
         offset = {50.f, 100.f};
-        offset.x = 40.f;
-        offset.y = 50.f;
+        offset.x = 50.f;
+        offset.y = 70.f;
                                 
         rotation = IsMouseButtonDown(MOUSE_LEFT_BUTTON) ? -85.f : -60.f;
     }
