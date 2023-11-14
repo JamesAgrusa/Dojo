d157a762a9e306013594dad22384c4808801c96d
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-11-13 19:04:23 -0500

    Add files via upload

--- BaseHero.cpp
+++ BaseHero.cpp
@@ -8,32 +8,5 @@ BaseHero::BaseHero()
 
 void BaseHero::tick(float deltaTime)
 {
-    if (Vector2Length(velocity) != 0.0)
-        {                  
-            worldPos = Vector2Subtract(worldPos, Vector2Scale(Vector2Normalize(velocity), speed));
-             velocity.x < 0.f ? rightLeft = 1.f : rightLeft = -1.f;
-             soldier = soldier_walk;
-        }
-        else
-        {
-            frame++;
-            runningTime = 0.f;
-           if (frame > maxFrames) frame = 4;
-            soldier = soldier_walk;
-        }
-        velocity = {};
-
-        runningTime += GetFrameTime();
-        if (runningTime >= updateTime)
-        {
-            frame++;
-            runningTime = 0.f;
-            if (frame > maxFrames) frame = 0;
-        }
-
-        // Draw the character
-
-        Rectangle source{frame * width, 0.f, rightLeft * width, height};
-        Rectangle dest{getScreenPos().x, getScreenPos().y, 4.0f * width, 4.0f * height};
-        DrawTexturePro(soldier, source, dest, Vector2{}, 0.f, WHITE);
+        
 }
