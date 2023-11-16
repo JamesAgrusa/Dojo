84f2f1e349825f675ac356e86695cc5ff4c0c4f7
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-11-15 23:23:49 -0500

    Add files via upload

--- BaseHero.cpp
+++ BaseHero.cpp
@@ -8,5 +8,27 @@ BaseHero::BaseHero()
 
 void BaseHero::tick(float deltaTime)
 {
-        
+    runningTime += deltaTime;
+    if (runningTime >= updateTime)
+    {
+        frame++;
+        runningTime = 0.f;
+        if (frame > maxFrames)
+            frame = 0;
+    }
+
+    if (Vector2Length(velocity) != 0.0)
+    {                  
+        worldPos = Vector2Subtract(worldPos, Vector2Scale(Vector2Normalize(velocity), speed));
+        velocity.x < 0.f ? rightLeft = 1.f : rightLeft = -1.f;
+        soldier = soldier_walk;
+    }
+    else
+    {
+        frame++;
+        runningTime = 0.f;
+        if (frame > maxFrames) frame = 4;
+        soldier = soldier_walk;
+    }
+    velocity = {};   
 }
