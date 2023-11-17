e669a5e9134d5e4a2ad6fb423e3f26ebfee92e93
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-11-16 23:34:55 -0500

    Add files via upload

--- Monster.cpp
+++ Monster.cpp
@@ -4,16 +4,17 @@
 Monster::Monster(Vector2 pos, Texture2D idle_texture, Texture2D run_texture)
 {
     worldPos = pos;
-    // texture = idle_texture;
-    // idle = idle_texture;
-    // run = run_texture;
-    // width = texture.width / maxFrames;
-    // height = texture.height;
+    texture = idle_texture;
+    idle = idle_texture;
+    run = run_texture;
+    width = texture.width / maxFrames;
+    height = texture.height;
     speed = 3.5f;
 }
 
 void Monster::tick(float deltaTime)
 {
+    getScreenPos() = Vector2Subtract(worldPos, target->getWorldPos());
     BaseHero::tick(deltaTime);
 }
 
