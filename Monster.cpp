4523f2d68319883bb492e97dc28af80f23bc6f18
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2024-01-10 23:15:44 -0500

    Add files via upload

--- Monster.cpp
+++ Monster.cpp
@@ -12,9 +12,16 @@ Monster::Monster(Vector2 pos, Texture2D idle_texture)
 
 void Monster::tick(float deltaTime)
 {
+    if (!getAlive()) return; 
+
     velocity = Vector2Subtract(target->getScreenPos(), getScreenPos());
     if (Vector2Length(velocity) < radius) velocity = {};
     BaseHero::tick(deltaTime);
+
+    if (CheckCollisionRecs(target->getCollisionRec(), getCollisionRec()))
+    {
+        target->takeDamage(damagePerSec * deltaTime);
+    }
 }
 
 Vector2 Monster::getScreenPos()
