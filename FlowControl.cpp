9934660581eccc7d0feffb85bea3c4a897076439
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2024-02-08 14:13:50 -0500

    Add files via upload

--- FlowControl.cpp
+++ FlowControl.cpp
@@ -1,16 +1,45 @@
 #include <iostream>
 #include <string>
-#include "LeaugeOfLegends.h"
-#include "CallOfDuty.h"
-#include "PC.h"
-#include "Playstation5.h"
-
 
 using namespace std;
 
+class LeagueOfLegends
+{
+public:
+	LeagueOfLegends();
+
+
+private:
+};
+
+class CallOfDuty
+{
+public:
+	CallOfDuty();
+
+private:
+};
+
+class PC
+{
+public:
+	PC();
+
+private:
+};
+
+class Playstation5
+{
+public:
+	Playstation5();
+
+private:
+};
+
 void GameSelection();
 void GameIntro();
-// bool WantToPlayAgain();
+void WeaponChoice();
+
 
 
 int main()
@@ -26,25 +55,28 @@ int main()
 void GameSelection()
 {
 
-	string pick;
-	char choice = 'A';
-
+	
+	char choice = (char ('A'), char ('B'));
+	
 	cout << "First, Which game would you like to play?\nEnter the letter following your pick:\nLeague of Legends(A) or Call of Duty(B)?\n";
-	cin >> pick;
-	switch (choice)
-		{
-		case 'A':
-			cout << "You Picked Leauge of Legends!\n" << endl;
-			cout << "Now that we know what game you want to play.\nIts time to pick a character!" << endl;
-			cout << "Here are your choices: " << endl;
-			break;
-			
-		case 'B':
-			cout << "You Picked Call of Duty!\n" << endl;
-			break;
-		default:
-			cout << "That is not a valid input" << endl;
-		}
+	cin >> choice;
+	if (choice == 'A')
+	{
+		cout << "You Picked Leauge of Legends!\n" << endl;
+		cout << "Now that we know what game you want to play.\nIts time to pick a character!" << endl;
+		cout << "You have three choices for a character: \n";
+		cout << "Ashe(C), Leona(D), or the obvious choice Taric(E)" << endl;
+		WeaponChoice();
+	}
+	else
+	{
+		cout << "You Picked Call of Duty!\n" << endl;
+		cout << "Now that we know what game you want to play.\nIts time to pick a weapon!" << endl;
+		cout << "You have three choices for a weapon: \n";
+		cout << "SMG(F), AR(G), Sniper(H)\n";
+
+	}
+	return ;
 }
 
 void GameIntro()
@@ -58,7 +90,11 @@ void GameIntro()
 	cout << "Todays activity is simple. This will be a Chose Your Own Adventure game! " << endl;
 }
 
-// bool WantToPlayAgain()
-// {
+void WeaponChoice()
+{
+	char weapon = (char('C'), char('D'), char('E'));
+	switch (weapon)
+			case 'C':
+				cout << "You Picked Ashe!\n";
+}
 
-// }
