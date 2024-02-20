9236eb9f46c0eb955348d0ac94d682c67ff848ed
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2024-02-20 06:37:54 -0500

    Add files via upload

---
+++ JamesBattle1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <ctime>
+#include <cmath>
+#include <cstdlib>
+#include "Arena.h"
+
+using namespace std;
+
+int main()
+{
+	
+    Arena arena;
+    arena.runGame();
+	
+	return 0;
+}
