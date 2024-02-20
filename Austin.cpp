a9874ab14c34c7587dcba99228c76b6d4094b197
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2024-02-20 18:19:37 -0500

    Add files via upload

---
+++ Austin.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Arena.h"
+#include "James.h"
+#include "Austin.h"
+
+using namespace std;
+
+Austin::Austin()
+{
+
+}
+
+void Austin::austinChoice()
+{
+	cout << "Sorry, you actually cant be Austin, you get it" << endl;
+	cout << "his power will simply render you useless. Dont worry, James 'probably' has this! " << endl;
+}
