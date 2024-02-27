c1f25991b7b6a65a38a5c100279b44f5e5eb4ce4
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2024-02-27 06:30:35 -0500

    Add files via upload

---
+++ Herd.cpp
@@ -0,0 +1,9 @@
+#include "Herd.h"
+#include "Dinosaur.h"
+
+Herd::Herd(int type, int herdHealth, int herdEnergy, int herdDef, int herdAttackPower)
+{
+	Dinosaur dino1(1, 100, 100, 100, 100);
+	Dinosaur dino2(1, 100, 100, 100, 100);
+	Dinosaur dino3(1, 100, 100, 100, 100);
+}
