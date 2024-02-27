9f717abbca3c8cf28d3d51b1d954367496c849c2
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2024-02-27 06:30:42 -0500

    Add files via upload

---
+++ Herd.h
@@ -0,0 +1,13 @@
+class Herd
+{
+public:
+	int type{};
+	int herdHealth{};
+	int herdEnergy{};
+	int herdDef{};
+	int herdAttackPower{};
+	int herdDamage{};
+
+	Herd(int type, int herdHealth, int herdEnergy, int herdDef, int herdAttackPower);
+	
+};
